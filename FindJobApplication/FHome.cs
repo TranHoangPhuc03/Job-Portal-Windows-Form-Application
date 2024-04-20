35962e8d0fbeafd3f1937e4e07090a70946d593d
HucPhoang <phuchoangtran2806@gmail.com>
2024-04-21 00:04:21 +0700

    commiting merge to master

--- FindJobApplication/FHome.cs
+++ FindJobApplication/FHome.cs
@@ -16,33 +16,20 @@ namespace FindJobApplication
     public partial class FHome : Form
     {
         UCHome uCHome = new UCHome();
-        UCMain uCMain = new UCMain();
         UCUserSubMenuRight uCUserSubMenuRight = new UCUserSubMenuRight();
 
         public FHome()
         {
             InitializeComponent();
-            this.Controls.Add(uCMain);
-            this.uCMain.BtnHome.Click += btnHome_Click;
-            this.uCMain.BtnUser.Click += btnUser_Click;
-            this.uCMain.BtnHome.PerformClick();
             customForUser();
         }
         private void customForUser ()
         {
-            uCMain.BtnUser.Text = "David Beckham";
-            uCMain.PbHomeUser.Image = Properties.Resources.man;
-            uCMain.PnlMid.Controls.Add(uCUserSubMenuRight);
-            uCUserSubMenuRight.Location = new Point(uCMain.PnlMid.Width - uCUserSubMenuRight.Width, 0);
             Anchor = AnchorStyles.Top | AnchorStyles.Right;
             uCUserSubMenuRight.hideMenu();
         }
         private void btnHome_Click(object sender, EventArgs e)
         {
-            uCMain.updateStatus(uCMain.BtnHome);
-            uCMain.PnlMid.Controls.Clear();
-            uCMain.PnlMid.Controls.Add(uCHome);
-            uCHome.Location = new Point(uCMain.PnlMid.Width / 2 - uCHome.Width / 2, 0);
             uCHome.BtnSearch.Click += jobFilterSearch;
         }
         private void btnUser_Click(object sender, EventArgs e)
@@ -67,6 +54,7 @@ namespace FindJobApplication
             this.uCHome.CbExperince.DataSource = yearExperienceDao.FindAllExperienceList();
 
             this.uCHome.fillJobPostToPanel(jobPostDao.FindAllJobPost());
+            this.pnlMain.Controls.Add(uCHome);
             this.Show();
         }
 
@@ -83,6 +71,11 @@ namespace FindJobApplication
                 dt = dt.Where(row => row.Title.Contains(keyword)).ToList();
             }
             this.uCHome.fillJobPostToPanel(dt);
+        }
+
+        private void pbUserImage_Click(object sender, EventArgs e)
+        {
+            this.tpnlUserNav.AutoSize ^= true;
         }
     }
 }
